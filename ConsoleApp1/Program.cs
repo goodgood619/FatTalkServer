e426d38c1b81810eb02015ee6632f0f3c1250562
goodgood619 <gktgnjftm@naver.com>
2019-07-19 13:58:19 +0900

    test

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -10,6 +10,7 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
+            // test
             MessengerServer messengerServer = new MessengerServer();
             Console.WriteLine("Server Start!");
             while (true)
